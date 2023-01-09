afcff5f380de443402817579248baea7574ddbc5
violetacf <violeta@cique.es>
2023-01-09 10:25:39 +0000

    Boilerplate for frontend

---
+++ src/Components/App/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Search from '../Search/Search';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">Food app coming...</header>
+      <Search />
+    </div>
+  );
+}
+
+export default App;

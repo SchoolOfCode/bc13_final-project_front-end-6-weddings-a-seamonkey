7729c545b5ebaf54fe7e69b0548ce1b5a2bbd7ce
marthamaybennett <marthamaybennett123@gmail.com>
2023-01-16 14:29:42 +0000

    Setting up about page

---
+++ src/Components/About/about.js
@@ -0,0 +1,94 @@
+import "./about.css";
+
+export default function About() {
+    <div class="about-section">
+        <h1>About FoodMap</h1>
+        <p>Some text about who we are and what we do.</p>
+        <p>Some more text about our brilliant app!</p>
+</div>
+
+<h2 style="text-align:center">Meet Our Team!</h2>
+<div class="row">
+  <div class="column">
+    <div class="card">
+      <img src="/Users/marthabennett/Documents/Coding/4weekproject/frontend/bc13_final-project_front-end-6-weddings-a-seamonkey/src/Images/Rachel.jpeg" alt="Rachel" style="width:100%">
+      <div class="container">
+        <h2>Rachel Morris</h2>
+        <p class="title">SoC Bootcamper</p>
+        <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+        <p>Rachel's GitHub link</p>
+        <p><button class="button">Find out more</button></p>
+      </div>
+    </div>
+  </div>
+</div>
+  <div class="row">
+    <div class="column">
+      <div class="card">
+        <img src="/w3images/team1.jpg" alt="Luke" style="width:100%">
+        <div class="container">
+          <h2>Luke Alexander</h2>
+          <p class="title">SoC Bootcamper</p>
+          <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+          <p>Luke's GitHub link</p>
+          <p><button class="button">Find out more</button></p>
+        </div>
+      </div>
+    </div>
+
+    <div class="row">
+        <div class="column">
+          <div class="card">
+            <img src="/Users/marthabennett/Documents/Coding/4weekproject/frontend/bc13_final-project_front-end-6-weddings-a-seamonkey/src/Images/Violeta.JPG" alt="Violeta" style="width:100%">
+            <div class="container">
+              <h2>Violeta Cique Fernández</h2>
+              <p class="title">SoC Bootcamper</p>
+              <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+              <p>Violeta's GitHub link</p>
+              <p><button class="button">Find out more</button></p>
+            </div>
+          </div>
+        </div>
+
+        <div class="row">
+            <div class="column">
+              <di class="card">
+                <img src="/Users/marthabennett/Documents/Coding/4weekproject/frontend/bc13_final-project_front-end-6-weddings-a-seamonkey/src/Images/Steve.jpeg" alt="Steven" style="width:100%">
+                <div class="container">
+                  <h2>Steven Collins</h2>
+                  <p class="title">SoC Bootcamper</p>
+                  <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+                  <p>Steven's GitHub link</p>
+                  <p><button class="button">Find out more</button></p>
+                </div>
+
+            </div>
+
+            <div class="row">
+                <div class="column">
+                  <div class="card">
+                    <img src="/w3images/team1.jpg" alt="Daniel" style="width:100%">
+                    <div class="container">
+                      <h2>Daniel Baillie</h2>
+                      <p class="title">SoC Bootcamper</p>
+                      <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+                      <p>Daniel's GitHub link</p>
+                      <p><button class="button">Find out more</button></p>
+                    </div>
+                  </div>
+                </div>
+
+                <div class="row">
+                    <div class="column">
+                      <div class="card">
+                        <img src="/Users/marthabennett/Documents/Coding/4weekproject/frontend/bc13_final-project_front-end-6-weddings-a-seamonkey/src/Images/Martha.jpg" alt="Martha" style="width:100%">
+                        <div class="container">
+                          <h2>Martha Bennett</h2>
+                          <p class="title">SoC Bootcamper</p>
+                          <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+                          <p>Martha's GitHub link</p>
+                          <p><button class="button">Find out more</button></p>
+                        </div>
+                      </div>
+                    </div>
+}
--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Search from "../Search/Search.js";
+import About from "../About/about";
 
 function App() {
 	return (
 		<div className="App">
 				<Search />
+				<About />
 		</div>
 	);
 }

988a72d3f58fb96ca6e42c050a9332ec38ab8f31
Sunbearian <stigcollins@icloud.com>
2023-01-16 11:29:47 +0000

    Map through the reason array

--- src/Components/Search/NegativeOutcome.js
+++ src/Components/Search/NegativeOutcome.js
@@ -8,14 +8,9 @@ export default function NegativeOutcome({ outcome }) {
 			<img className="face" src={sadFace} alt="Sad emoji face" />
 			<p>Don't eat {outcome.productName}!</p>
 			<>Product contains:</>
-			<p>{outcome.reason[0]}</p>
-			<p>{outcome.reason[1]}</p>
-			<p>{outcome.reason[2]}</p>
-			{/* <>{glutenResult}</>
-			<br></br>
-			<>{fodmapResult}</>
-			<br></br>
-			<>{lactoseResult}</> */}
+			{outcome.reason.map((reason) => {
+				return <li>{reason}</li>;
+			})}
 		</div>
 	);
 }

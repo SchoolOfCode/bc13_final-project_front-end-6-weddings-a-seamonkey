f28c72c7f66b5b49cc58ab960febe20cb3422e59
Rachel Morris <107057925+rachvm@users.noreply.github.com>
2023-01-17 09:17:19 +0000

    Frontend (#19)
    * Boilerplate for frontend
    
    * Updated readme with dev instructions
    
    * Updated readme.md and added CSS variables
    
    * Images added
    
    * Created search inputs and postive & negative outcome components
    
    * Functionality for useState added - review functions
    
    * connected back and front end for MVP
    
    * if
    
    * created if statements to handle pos or neg outcome
    
    * added url variable for fetch request
    
    * working on frontend bugs
    
    * Search (#6)
    
    * refactoring functions
    
    * fixed search logic problem
    
    * working through logic of search function
    
    Co-authored-by: violetacf <violeta@cique.es>
    
    * Css (#7)
    
    * setting up search
    
    * Added css variables
    
    * slider
    
    * added gluten free slider
    
    * Toggles added and search bar responsive
    
    * emjois
    
    * tiding the CSS
    
    Co-authored-by: Rachel Morris <rachelmorris262@outlook.com>
    Co-authored-by: Sunbearian <stigcollins@icloud.com>
    
    * fixed for deployment
    
    * positiveoutcome displays productname
    
    * props render on 2nd click
    
    * default outcome added, working on showing not to eat reasons
    
    * Search improvement (#10)
    
    * adding search button
    
    * Created a search button
    
    * Took hover off as doesnt play well with iOS
    
    Co-authored-by: marthamaybennett <marthamaybennett123@gmail.com>
    
    * Re added button
    
    * Tweaked the css
    
    * Fix logic (#15)
    
    * Fixed the reason display list
    
    * Removed obsolete state and comments
    
    * Map through the reason array
    
    * added ket
    
    * added no product error and loading message
    
    * Won't accept empty input
    
    Co-authored-by: luke123adams <lxa207@gmail.com>
    
    * completed side menu
    
    * setting up header
    
    * bottom bar completed css added to menu
    
    Co-authored-by: violetacf <violeta@cique.es>
    Co-authored-by: Sunbearian <stigcollins@icloud.com>
    Co-authored-by: MarthaBennett <108686420+MarthaBennett@users.noreply.github.com>
    Co-authored-by: luke123adams <lxa207@gmail.com>
    Co-authored-by: Steven Collins <91161452+Sunbearian@users.noreply.github.com>
    Co-authored-by: marthamaybennett <marthamaybennett123@gmail.com>
    Co-authored-by: luke123adams <112545296+luke123adams@users.noreply.github.com>

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,11 +1,23 @@
 import "./App.css";
-import Search from "../Search/Search.js";
+import TemporaryDrawer from "../../Navigation/menu/index.js";
+import { Outlet } from "react-router";
+import FixedBottomNavigation from "../../Navigation/bottombar/index.js";
+
 
 function App() {
 	return (
-		<div className="App">
-				<Search />
+		<>
+		<div className="Menu">
+			<h1 className="title">FoodMap</h1>
+			<TemporaryDrawer className="button"/>
 		</div>
+		<div className="App">	
+			<Outlet/>
+		</div>
+		<div className="bar">
+		<FixedBottomNavigation/>
+		</div>
+		</>
 	);
 }
 

5332bbaeeebff3257d4a0ae6ae7dfc2532b0ff4d
marthamaybennett <marthamaybennett123@gmail.com>
2023-01-17 15:53:56 +0000

    updating css to improve alignment and font colour

--- src/Components/Instructions/index.js
+++ src/Components/Instructions/index.js
@@ -7,82 +7,33 @@ import sadFace from '../../Images/sad.gif';
 
 function Instructions() {
 	return (
-		<div>
-		
-			<div>
+		<div className="instructions">
+			
 			<h1 className="heading">How to use FoodMap</h1>
-			</div>
+			
+			<h2 className="subheading">What does the app do?</h2>
+			<p className="paragraph">This app is designed to help you navigate complex dietary requirements in a supermarket setting.</p>
 
-				<div>
-				<h2 className="subheading">What does the app do?</h2>
-				<p>This app is designed to help you navigate complex dietary requirements in a supermarket setting.</p>
-				</div>
+			<h2 className="subheading">How do I use it?</h2>
+			<p className="paragraph">1. You can either type the name of the product or the barcode number into the search bar:</p>
+			<img src={searchBar} alt="search bar" className="search-bar"></img>
+			<p className="paragraph">2. Select your dietary requirements:</p>
+			<img src={dietaryRequirements} alt="dietary requirements" className="dietary-requirements"></img>
+			<p className="paragraph">3. Click the 'Can I Eat This?' button</p>
 
-					<div>
-					<h2>How do I use it?</h2>
-					<p>1. You can either type the name of the product or the barcode number into the search bar:</p>
-					</div>
+			<h2 className="subheading">Or, you can use the Barcode Scanner to scan the barcode of each product:</h2>
+			<p className="paragraph">1. Select your dietary requirements:</p>
+			<img src={dietaryRequirements} alt="dietary requirements" className="dietary-requirements"></img>
+			<p className="paragraph">2. Click on the Barcode Scanner icon at the bottom of your screen</p>
+			<img src={barcodeScanner} alt="barcode scanner" className="barcode-scanner"></img>
+			<p className="paragraph">3. Use the Barcode Scanner thus*:</p>
 
-						<div>
-						<img src={searchBar} alt="search bar"></img>
-						</div>
-
-							<div>
-							<p>2. Select your dietary requirements:</p>
-							</div>
-
-								<div>
-								<img src={dietaryRequirements} alt="dietary requirements"></img>
-								</div>
-							
-									<div>
-									<p>3. Click the 'Can I Eat This?' button</p>
-									</div>
-
-										<div>
-										<h2>Or, you can use the Barcode Scanner to scan the barcode of each product:</h2>
-										</div>
-
-											<div>
-											<p>1. Select your dietary requirements:</p>
-											</div>
-
-												<div>
-												<img src={dietaryRequirements} alt="dietary requirements"></img>
-												</div>
-
-													<div>
-													<p>2. Click on the Barcode Scanner icon at the bottom of your screen</p>
-													</div>
-
-														<div>
-														<img src={barcodeScanner} alt="barcode scanner"></img>
-														</div>
-
-															<div>
-															<p>3. Use the Barcode Scanner thus*:</p>
-															</div>
-
-																<div>
-																<h2>Results:</h2>
-																</div>
-
-																	<div>
-																	<p>If you can eat this product, a happy face will appear:</p>
-																	</div>
-
-																		<div>
-																		<img src={happyFace} alt="happy face"></img>
-																		</div>
-
-																			<div>
-																			<p>If you can't eat this product, a sad face will appear:</p>
-																			</div>
-
-																				<div>
-																				<img src={sadFace} alt="sad face"></img>
-																				</div>
-
+			<h2 className="subheading">Results:</h2>
+			<p className="paragraph">If you can eat this product, a happy face will appear:</p>
+			<img src={happyFace} alt="happy face" className="happy-face"></img>
+			<p className="paragraph">If you can't eat this product, a sad face will appear:</p>
+			<img src={sadFace} alt="sad face" className="sad-face"></img>
+			<p className="paragraph">Happy shopping!</p>																					
 		</div>
 	);
 }

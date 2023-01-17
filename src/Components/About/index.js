60e5ca964b6eb49401bfb42cbcdd142fbb0ad898
Rachel Morris <rachelmorris262@outlook.com>
2023-01-17 11:18:06 +0000

    updated with about page into most updated version

--- src/Components/About/index.js
+++ src/Components/About/index.js
@@ -1,7 +1,68 @@
-function About() {
-	return (
-		<>About Content to be added ...</>
-	);
-}
+import "./about.css";
 
-export default About;
+export default function About() {
+    return (
+<div>
+    <div className="about-section">
+        <h1>Why FoodMap?</h1>
+
+        <h2>Do you find it difficult to shop for your dietary requirements?</h2>
+
+        <p>Over 15 million people in the UK live with a chronic health condition. Some of these conditions are managed using dietary restrictions.</p>
+        
+        <p>But what if your dietary restrictions are not, technically, allergens? Packaging can be hard to interpret and there are so many variables to consider, that shopping for even basic foodstuffs can be overwhelming.</p>
+
+        <h2>That's where FoodMap comes in!</h2>
+
+        <p>FoodMap is designed to help you work out whether the food you're buying works with your dietary requirements. Whether you have Crohn's, Ulcerative Colitis, Diverticulitis, Endometriosis, IBS (or any gut-impacted health conditions), following a specific diet as set out by your GP or a Dietician can help.</p>
+
+        <p>Head to our 'Instructions' page to find out how to use FoodMap</p>
+
+        <p>Please note: FoodMaps is not intended as a medical or diagnostic device. Always follow the guidance of your GP or a Dietician when following a restrictive or limiting diet.</p>
+        
+    </div>
+
+        <div>
+            <h2>Who are FoodMap?</h2>
+                <p>FoodMap was developed by a team of Bootcampers from School of Code as a 4 week project.</p>
+        </div>
+
+        <h2>Meet Our Team!</h2>
+    
+            <h2>Rachel Morris</h2>
+            <p className="title">SoC Bootcamper</p>
+            <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+            <p><button className="button">Find out more</button></p>
+    
+
+            <h2>Luke Alexander</h2>
+            <p className="title">SoC Bootcamper</p>
+            <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+            <p><button className="button">Find out more</button></p>
+   
+
+                <h2>Violeta Cique Fernández</h2>
+                <p className="title">SoC Bootcamper</p>
+                <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+                <p><button className="button">Find out more</button></p>
+         
+
+                    <h2>Steven Collins</h2>
+                    <p className="title">SoC Bootcamper</p>
+                    <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+                    <p><button className="button">Find out more</button></p>
+               
+
+                        <h2>Daniel B</h2>
+                        <p className="title">SoC Bootcamper</p>
+                        <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+                        <p><button className="button">Find out more</button></p>
+               
+            
+                            <h2>Martha Bennett</h2>
+                            <p className="title">SoC Bootcamper</p>
+                            <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+                            <p>Martha's GitHub link</p>
+                            <p><button className="button">Find out more</button></p>
+            
+</div> )}

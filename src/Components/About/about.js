06094088b63fee2658476fe876f219b3a5358c9e
marthamaybennett <marthamaybennett123@gmail.com>
2023-01-19 09:28:53 +0000

    stashed grid and tweaking margin-bottom

--- src/Components/About/about.js
+++ src/Components/About/about.js
@@ -25,7 +25,7 @@ export default function About() {
 
         <p>But what if you have been advised to follow a low FODMAP diet?</p>
         
-        <p>You will be asked to avoid ingredients that are high in FODMAPs. This includes not only higlighted allergens such as <strong>wheat,</strong> <strong>milk</strong> and <strong>celery</strong>, but also ingredients that are not highlighted in bold.</p>
+        <p>You will be asked to avoid ingredients that are high in FODMAPs. This includes not only highlighted allergens such as <strong>wheat,</strong> <strong>milk</strong> and <strong>celery</strong>, but also ingredients that are not highlighted in bold.</p>
         
         <p>Foods which are high in FODMAPs are underlined in red:</p>
 
@@ -48,38 +48,30 @@ export default function About() {
     
             <h3>Rachel Morris</h3>
             <p>SoC Bootcamper</p>
-
             <p>Some text that describes me lorem ipsum ipsum lorem.</p>
     
 
             <h3>Luke Alexander</h3>
-
             <p>SoC Bootcamper</p>
             <p>Luke spent five years in the civil service working as a debt management adviser before deciding to make a career switch and re-training as a software developer. He wants to use the communication skills gained from thousands of difficult conversations with taxpayers and apply them in an exciting field where he can collaborate on projects that make a difference in the world.</p>
    
 
             <h3>Violeta Cique Fernández</h3>
             <p>SoC Bootcamper</p>
-
-            <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+            <p>With a background in Primary Teaching Education and P.E., Violeta worked for almost six years as a SEND Tutor with young people on the Autism Spectrum before switching careers to software developing. She wants to use her experience to make a difference towards making sites more accessible for all kinds of users.</p>
          
 
             <h3>Steven Collins</h3>
-
             <p>SoC Bootcamper</p>
-
             <p>Some text that describes me lorem ipsum ipsum lorem.</p>
                
 
             <h3>Daniel B</h3>
-
             <p>SoC Bootcamper</p>
-
-            <p>Some text that describes me lorem ipsum ipsum lorem.</p>
+            <p>Dan is a coffee addict and video gamer with a passion for Horror and food. After studying law and working in social media he decided to listen to his brain and switch to a career that would challenge him and make him happy.</p>
                
             
             <h3>Martha Bennett</h3>
-
             <p>SoC Bootcamper</p>
             <p>Martha studied Education at University before launching her own education business in China and Hong Kong. Six years on, she has worked with some great developers and is curious to find out more about coding</p>
 

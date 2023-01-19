a9a46ecf85ca78699df5398fcf34c47b2f08eb60
Cmndgrab <110107052+Cmndgrab@users.noreply.github.com>
2023-01-19 10:16:12 +0000

    routing and authentication buttons added and tested

---
+++ src/Components/List/List.js
@@ -0,0 +1,6 @@
+
+
+
+export default function List(){
+    return <h1>Hello world its me margaret</h1>
+}
--- src/index.js
+++ src/index.js
@@ -13,6 +13,7 @@ import About from './Components/About/index.js';
 import Instructions from './Components/Instructions/index.js';
 import AuthenticationButton from './Components/Login-Logout/AuthenticationButton.js';
 import LoginButton from './Components/Login-Logout/LoginButton.js';
+import List from './Components/List/List.js';
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route element={<App />}>
@@ -20,7 +21,7 @@ const router = createBrowserRouter(
       <Route path="/about" element={<About />} />
       <Route path="/instructions" element={<Instructions />} />
       <Route path="/Login" element={<AuthenticationButton />} />
-      <Route path="/List" element={<AuthenticationButton />} />
+      <Route path="/List" element={<List/>} />
     </Route>
   )
 );

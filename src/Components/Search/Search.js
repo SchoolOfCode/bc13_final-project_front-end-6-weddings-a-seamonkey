6321ea2f6828f843a9b58f0b07115d8a3635465a
marthamaybennett <marthamaybennett123@gmail.com>
2023-01-20 10:36:46 +0000

    added barcode icon to search and centred button

--- src/Components/Search/Search.js
+++ src/Components/Search/Search.js
@@ -2,6 +2,7 @@ import NegativeOutcome from "./NegativeOutcome.js";
 import PositiveOutcome from "./PositiveOutcome.js";
 import DefaultOutcome from "./DefaultOutcome.js";
 import magnifying from "../../Images/magnifying-dark.png";
+import barcodeScan from "../../Images/barcode-scan.png";
 import "./Search.css";
 import { useState } from "react";
 import { Cameraswitch } from "@mui/icons-material";
@@ -116,11 +117,11 @@ export default function Search() {
 				<img src={magnifying} alt="Magnifying glass" onClick={onClick} />
 				<input
 					type="text"
-					placeholder="Find by food or barcode"
+					placeholder="Search food or barcode"
 					onChange={onChange}
 					value={search.searchTerm}
 				></input>
-				<button onClick={switchBarcode}>Barcode?</button>
+				<img src={barcodeScan} alt="barcode scan icon" onClick={switchBarcode}></img>
 			</div>
 			{barcodeScanner === false ? (
 				<div>
@@ -156,10 +157,11 @@ export default function Search() {
 							</label>
 							<span>Lactose Free</span>
 						</div>
-					</div>
-					<button className="search-button" onClick={onClick}>
+						<button className="search-button" onClick={onClick}>
 						Can I eat this?
 					</button>
+					</div>
+					
 					{loadingSearch === true ? (
 						<p className="loading-msg">Loading...</p>
 					) : (

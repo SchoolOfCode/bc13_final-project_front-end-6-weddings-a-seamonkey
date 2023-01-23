88ad493ccf25332cee619ebb50e0f4ad4cf24990
violetacf <violeta@cique.es>
2023-01-23 14:36:12 +0000

    style for delete buttons - need :active

--- src/Components/List/List.js
+++ src/Components/List/List.js
@@ -1,37 +1,59 @@
-import {useEffect, useState} from 'react'
-import { useAuth0 } from "@auth0/auth0-react";
-const url = process.env.REACT_APP_SERVER_URL ?? "http://localhost:3010";
-export default function List(){
+import { useEffect, useState } from 'react';
+import { useAuth0 } from '@auth0/auth0-react';
+const url = process.env.REACT_APP_SERVER_URL ?? 'http://localhost:3010';
+export default function List() {
+  const [array, setArray] = useState([]);
+  const { user } = useAuth0();
+  const { sub } = user;
 
-    const [array,setArray] =useState([])
-    const { user } = useAuth0()
-    const {sub} = user
+  async function DeleteFromList(item) {
+    const [active, setActive] = useState(false);
+    const response = await fetch(
+      `${url}/api/userproducts/${sub}/${item.product_name}`,
+      { method: 'DELETE' }
+    );
+    console.log(sub);
+    console.log(item.product_name);
+    GetList();
+  }
 
-    async function DeleteFromList(item){
-        const response = await fetch (`${url}/api/userproducts/${sub}/${item.product_name}`,{method: 'DELETE'})
-        console.log(sub)
-        console.log(item.product_name)
-        GetList()
-    }
-
-    async function GetList(){
+  async function GetList() {
     const response = await fetch(`${url}/api/userproducts/${sub}`);
-			const data = await response.json();
-			const payload = data.payload;
-            setArray(payload)
-            }
+    const data = await response.json();
+    const payload = data.payload;
+    setArray(payload);
+  }
 
-            useEffect(()=>{GetList()},[])
-            
+  useEffect(() => {
+    GetList();
+  }, []);
 
-    return (<><h1>My Foods</h1>
+  return (
+    <>
+      <h1>My Foods</h1>
 
-
-            {array.map((item,index)=>{return <li key={index}>{item.product_name}<button className="delete-button" onClick={()=>{DeleteFromList(item)}}>Delete from list</button></li>})}
-
-
-            </>)
-            
-            
-            
-    }        
+      {array.map((item, index) => {
+        return (
+          <li key={index}>
+            {item.product_name}
+            <button
+              style={{
+                borderRadius: '15px',
+                backgroundColor: 'var(--font-color)',
+                padding: '5px 20px',
+                color: '#f0ffff',
+                marginBottom: '20px',
+                fontSize: '16px',
+              }}
+              onClick={() => {
+                DeleteFromList(item);
+              }}
+            >
+              Delete from list
+            </button>
+          </li>
+        );
+      })}
+    </>
+  );
+}

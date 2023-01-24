13611d9434c763f651b84ea60076404d5d79f03f
Rachel Morris <rachelmorris262@outlook.com>
2023-01-24 17:27:43 +0000

    icons added on menu bar

--- src/Navigation/menu/index.js
+++ src/Navigation/menu/index.js
@@ -1,16 +1,20 @@
 import * as React from 'react';
 import { Link } from 'react-router-dom';
 import './style.css'
-import { Box, Button, Drawer, List, ListItem, Divider } from '@mui/material';
+import { Box, Button, Drawer, List, ListItem, ListItemIcon, Divider } from '@mui/material';
 import { Dehaze } from '@mui/icons-material';
 import { Auth0Provider } from '@auth0/auth0-react';
 import AuthenticationButton from '../../Components/Login-Logout/AuthenticationButton.js';
-
+import { Appicons } from './appicons.js';
 import { useAuth0 } from '@auth0/auth0-react';
+import { useNavigate } from 'react-router-dom';
+import logo from '../../Images/logo.png'
+import { Login, Logout, FoodBank } from '@mui/icons-material';
 
 export default function TemporaryDrawer() {
   const { isAuthenticated } = useAuth0();
   const [state, setState] = React.useState(false);
+  const navigate = useNavigate();
 
   const toggleDrawer = (anchor, open) => (event) => {
     if (event.type === 'keydown') {
@@ -22,34 +26,51 @@ export default function TemporaryDrawer() {
 
   const list = (anchor) => (
     <Box
-      sx={{ width: 250}}
+      sx={{ width: 300}}
       onClick={toggleDrawer(anchor, false)}
       
     >
+      <Divider sx={{backgroundColor: "var(--menu-bar)"}}/>
+      <div className="thedivide">
+            <h3 className="label">FoodMap</h3>
+            <img
+              className="thelogo"
+              src={logo}
+              alt="logo"
+            ></img>
+            </div>
+      <Divider sx={{backgroundColor: "var(--menu-bar)"}}/>
+        <List >
+          {Appicons.map((element) => (
+            <ListItem key={element.id}>
+              <Button onClick={() => navigate(element.route)} 
+                  sx={{
+                    color: "var(--font-color)",
+                    textDecoration: "none",
+                    fontSize: "large",
+                    fontWeight: "bold",
+                    fontFamily: "Verdana, Geneva, Tahoma, sans-serif"}} startIcon={<ListItemIcon sx={{color: "var(--font-color)", fontSize: "large"}}>{element.icon}</ListItemIcon>}>{element.label}</Button>
+            </ListItem>
+          ))}
+        </List>
+      <Divider sx={{backgroundColor: "var(--menu-bar)"}}/>
+      
       <List >
-
-        {[{id:0, label:'Search', route:"/"}, {id:1, label:'About', route:"/about"}, {id:2, label:'Instructions', route:"/instructions"}].map((text) => (
-
-
+        {[isAuthenticated ?{id:3, label:'Logout', icon: <Logout/>, route:"/Login"}:{id:4, label:'Login', icon: <Login/>, route:"/Login"} , isAuthenticated ?{id:4, label:'My Foods', icon: <FoodBank/>,route:"/Myfoods"} : {}].map((text) => (
           <ListItem key={text.id}>
-            <Link className="label" to={text.route}>{text.label}</Link>
+            <Button className="label" onClick={() => navigate(text.route)} 
+              sx={{
+                    color: "var(--font-color)",
+                    textDecoration: "none",
+                    fontSize: "large",
+                    fontWeight: "bold",
+                    fontFamily: "Verdana, Geneva, Tahoma, sans-serif"}} startIcon={<ListItemIcon sx={{color: "var(--font-color)"}}>{text.icon}</ListItemIcon>} >
+              {text.label}
+            </Button>
           </ListItem>
         ))}
       </List>
       <Divider sx={{backgroundColor: "var(--menu-bar)"}}/>
-      
-      <List >
-
-
-        {[isAuthenticated ?{id:3, label:'Logout',route:"/Login"}:{id:4, label:'Login',route:"/Login"} , isAuthenticated ?{id:4, label:'My Foods',route:"/Myfoods"} : {}].map((text) => (
-
-          <ListItem key={text.id}>
-            <Link className="label" to={text.route}>
-              {text.label}
-            </Link>
-          </ListItem>
-        ))}
-      </List>
     </Box>
   );
 
@@ -66,6 +87,7 @@ export default function TemporaryDrawer() {
               <Dehaze sx={{ fontSize: 'xx-large' }} />
             </Button>
           </Box>
+          
           <Drawer
             anchor={anchor}
             open={state[anchor]}

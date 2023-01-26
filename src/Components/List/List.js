251a5f3e5569d31a70d121ad3369ac97f697d62b
Sunbearian <stigcollins@icloud.com>
2023-01-26 09:46:46 +0000

    Message when list is empty

--- src/Components/List/List.js
+++ src/Components/List/List.js
@@ -26,6 +26,7 @@ export default function List() {
 		const response = await fetch(`${url}/api/userproducts/${sub}`);
 		const data = await response.json();
 		const payload = data.payload;
+		console.log(payload, "this is the list payload");
 		setArray(payload);
 	}
 
@@ -37,33 +38,36 @@ export default function List() {
 	return (
 		<>
 			<h1>My Foods</h1>
-
-			<div className="list-items">
-				{array.map((item, index) => {
-					return (
-						<ul>
-							<li key={index}>
-								<div className="list-item">
-									<img
-										className="list-image"
-										src={logo}
-										alt="foodmap-logo"
-									></img>
-									<p className="product-name">{item.product_name}</p>
-								</div>
-								<button
-									className="delete-button"
-									onClick={() => {
-										DeleteFromList(item);
-									}}
-								>
-									Delete from list
-								</button>
-							</li>
-						</ul>
-					);
-				})}
-			</div>
+			{array.length === 0 ? (
+				<h3>Scanned foods can be added to this list</h3>
+			) : (
+				<div className="list-items">
+					{array.map((item, index) => {
+						return (
+							<ul>
+								<li key={index}>
+									<div className="list-item">
+										<img
+											className="list-image"
+											src={logo}
+											alt="foodmap-logo"
+										></img>
+										<p className="product-name">{item.product_name}</p>
+									</div>
+									<button
+										className="delete-button"
+										onClick={() => {
+											DeleteFromList(item);
+										}}
+									>
+										Delete from list
+									</button>
+								</li>
+							</ul>
+						);
+					})}
+				</div>
+			)}
 		</>
 	);
 }
